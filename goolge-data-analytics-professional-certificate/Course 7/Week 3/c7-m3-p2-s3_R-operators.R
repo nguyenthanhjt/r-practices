446923c25bb62070afb5ab77fcff437343299a6c
Thanh Bui Nguyen <nguyenthanhjt@gmail.com>
2024-03-20 13:38:31 +0700

    Create c7-m3-p2-s3_R-operators.R

---
+++ goolge-data-analytics-professional-certificate/Course 7/Week 3/c7-m3-p2-s3_R-operators.R
@@ -0,0 +1,35 @@
+x <- 2
+y <- 5
+
+# Arithmetic operators
+x + y
+x - y
+x * y
+x / y
+y %% x
+y %/% x
+y ^ x
+
+# Relational operators
+x < y
+x > y
+x <= 2
+y >= 10
+y == 5
+x != 2
+
+# Logical operators
+
+z <- 10
+
+z > 2 & z < 12
+
+!(x<15)
+
+# Assginment Operator
+
+x <- 2  
+x <<- 7 
+x = 9   
+11 -> x 
+21 ->> x
