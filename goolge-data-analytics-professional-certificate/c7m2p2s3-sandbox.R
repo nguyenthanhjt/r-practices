a5f98194cc5eea2d5fbbb828f722a37e312d8f10
Thanh Bui Nguyen <nguyenthanhjt@gmail.com>
2024-03-20 13:38:13 +0700

    Delete c7m2p2s3-sandbox.R

--- goolge-data-analytics-professional-certificate/c7m2p2s3-sandbox.R
+++
@@ -1,8 +0,0 @@
-install.packages("tidyverse")
-library(tidyverse)
-head(diamonds)
-
-ggplot(data = diamonds, aes (x = carat, y=price)) + geom_point()
-ggplot(data = diamonds, aes(x = carat, y=price, color=cut)) + geom_point() 
-
-ggplot(data=diamonds, aes(x=carat, y=price, color=cut)) + geom_point()+facet_wrap(~cut)
