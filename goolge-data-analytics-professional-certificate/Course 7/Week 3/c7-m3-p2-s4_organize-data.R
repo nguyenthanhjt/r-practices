05112721705d6af04db7757d9bac4955fd032e0b
Thanh Bui Nguyen <nguyenthanhjt@gmail.com>
2024-03-20 15:01:27 +0700

    Create c7-m3-p2-s4_organize-data.R

---
+++ goolge-data-analytics-professional-certificate/Course 7/Week 3/c7-m3-p2-s4_organize-data.R
@@ -0,0 +1,39 @@
+# load data `palmer-penguins`
+install.packages("palmerpenguins")
+library("palmerpenguins")
+
+library(tidyverse)
+
+penguins %>% arrange(bill_length_mm) # sort ASC
+
+penguins %>% arrange(-bill_length_mm) # sort DESC
+
+# store in DF
+penguins2 <- penguins %>% arrange(-bill_length_mm)
+
+head(penguins2)
+
+# group by
+## group by island, summarize
+
+### get average of bill length
+penguins %>%
+  group_by(island) %>% #
+  drop_na() %>% # remove na rows
+  summarize(mean_bill_length_mm = mean(bill_length_mm))
+
+### get maximum bill length
+penguins %>%
+  group_by(island) %>%
+  drop_na() %>%
+  summarise(maximum_bill_lenth_mm = max(bill_length_mm))
+
+# groupy by multiple columns
+penguins %>% 
+  group_by(island, species) %>% 
+  drop_na() %>% 
+  summarise(max_bl = max(bill_length_mm), mean_bl = mean(bill_length_mm))
+
+# filtering
+
+penguins %>% filter(species == 'Adelie')
